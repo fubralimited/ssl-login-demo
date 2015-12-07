5fd76d682d91cec6729e206d39732ab80860df77
Ben Kennish <ben@kennish.net>
2015-12-07 12:30:13 +0000

    Remove method that no longer works on Firefox

--- logging-out.php
+++ logging-out.php
@@ -43,9 +43,11 @@ function logOut()
 
 <h2>Firefox <?php if ($ua['name'] == 'firefox') echo ' &larr; your browser'; ?></h2>
 
+<!--
 <p><strong>Easiest method</strong> : <a href="javascript:window.crypto.logout(); window.opener.location.reload(false); window.close();">Click here to logout</a> (uses <a href="https://developer.mozilla.org/en/docs/JavaScript_crypto" target="_blank">the window.crypto Mozilla extension</a>.)</p>
+-->
 
-<p><strong>Alternative method</strong> : click 'History' menu (press ALT key to display menu if it's not there) &rarr; 'Clear Recent History' &rarr; tick only 'Active Logins' and click 'Clear Now' button. Then <a href="javascript: window.opener.location.reload(false); window.close();">close this page and reload the previous page</a>.</p>
+<p><strong>Method</strong> : click 'History' menu (press ALT key to display menu if it's not there) &rarr; 'Clear Recent History' &rarr; tick only 'Active Logins' and click 'Clear Now' button. Then <a href="javascript: window.opener.location.reload(false); window.close();">close this page and reload the previous page</a>.</p>
 
 <h2>Chrome / Safari <?php if ($ua['name'] == 'chrome' || $ua['name'] == 'safari') echo ' &larr; your browser'; ?></h2>
 <p>Try clicking <a href="#logout" onclick="logOut();">logout in background</a> multiple times until you are prompted to choose a certificate. When prompted for a certificate, click "Cancel" then <a href="javascript: window.opener.location.reload(false); window.close();">close this page and reload the previous page</a>.</p>
